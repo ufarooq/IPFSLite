2b88da307e12ea203801a506583d2fcb0c689326
Remmer Wilts <remmer.wilts@gmail.com>
2019-09-16 19:08:36 +0200

    Version 1.0.4

--- app/src/main/java/threads/server/SwarmFragment.java
+++ app/src/main/java/threads/server/SwarmFragment.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -244,7 +243,6 @@ public class SwarmFragment extends Fragment implements
 
         });
         loadPeers();
-        Toast.makeText(mContext, R.string.drag_down_refresh, Toast.LENGTH_LONG).show();
 
         return view;
     }
