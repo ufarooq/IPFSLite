0809a210d7f99980c7e79154c2c2bb6cbf37ef2e
remmerw <remmer.wilts@gmail.com>
2020-02-28 10:07:20 +0100

    Init Version

--- app/src/main/java/threads/server/core/events/EventsDatabase.java
+++ app/src/main/java/threads/server/core/events/EventsDatabase.java
@@ -2,7 +2,7 @@ package threads.server.core.events;
 
 import androidx.room.RoomDatabase;
 
-@androidx.room.Database(entities = {Event.class}, version = 111, exportSchema = false)
+@androidx.room.Database(entities = {Event.class}, version = 112, exportSchema = false)
 public abstract class EventsDatabase extends RoomDatabase {
 
     public abstract EventDao eventDao();
--- app/src/main/java/threads/server/core/peers/PeersDatabase.java
+++ app/src/main/java/threads/server/core/peers/PeersDatabase.java
@@ -2,7 +2,7 @@ package threads.server.core.peers;
 
 import androidx.room.RoomDatabase;
 
-@androidx.room.Database(entities = {Peer.class}, version = 111, exportSchema = false)
+@androidx.room.Database(entities = {Peer.class}, version = 112, exportSchema = false)
 public abstract class PeersDatabase extends RoomDatabase {
     public abstract PeerDao peersDao();
 }
